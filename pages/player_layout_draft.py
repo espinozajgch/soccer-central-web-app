2534ed4a271a5ef557c24d99855fca70a4db62fa
Francisco J <siscomagma@gmail.com>
2025-05-27 14:00:00 -0600

    Fix para poder usar SQLAlchemy 360

--- pages/player_layout_draft.py
+++ pages/player_layout_draft.py
@@ -2,29 +2,15 @@ import streamlit as st
 from utils import login
 import pandas as pd
 import numpy as np
-import pymysql
-pymysql.install_as_MySQLdb()
 from datetime import datetime
+from db import SessionLocal
+from models import Users, Players, PlayerGameStats
 
-
-
-# Función para inicializar la conexión a la base de datos y cachearla
-def connect_to_db():
-    try:
-        # Intentamos obtener la conexión definida en los secrets
-        conn = st.connection('mysql', type='sql')
-        st.success("Conexión establecida correctamente")
-    except Exception as e:
-        # Si ocurre algún error, lo capturamos y mostramos un mensaje en la aplicación
-        st.error("Error al conectar con la base de datos:")
-        st.error(e) 
-    return conn
 
 
 
 
 def Setup_page():
-    login.generarLogin()
     # Configuración de la página
     # Configuración título de página.
     # st.set_page_config(page_title="Player Layout Draft", page_icon=":soccer:", 
@@ -79,68 +65,37 @@ def Show_Player_Info():
     #Para la versión actual (MVP) el módulo construido muestra la posible posición de información básica y del perfil del jugador.
     #Versiones posteriores podrían facilitar una configuración personalizable por el usuario.
     # Se espera revisión de contenido y posible look and feel con el cliente para la versión final.
-    
-    # Conexión a DB*******************
-    dbconn = connect_to_db()
        
     #Preparación de la página**********************************************************************
     st.header("360° PLAYER DATA LAYOUT", divider="gray")
+    session = SessionLocal()
     # Mostrar la foto del jugador.
     #st.image("https://media.gettyimages.com/id/1365815844/es/foto/mexico-city-mexico-argentina-captain-diego-maradona-pictured-before-the-fifa-1986-world-cup.jpg?s=612x612&w=gi&k=20&c=vmUfSD2BY0_TQqFi8btORJj6OlNIBTvhkq1RrsY9kV4=", width=300)
     
    
     # El Usuario podrá escoger el jugador por el player_id.
     # Obtener la lista de player_id desde la tabla players
-    df_plyid = dbconn.query("SELECT player_id FROM players", ttl=3600)
-    player_ids = df_plyid["player_id"].tolist()
+
+    df_players = session.query(Players).limit(10).all()
+    player_ids = [player.player_id for player in df_players]
     # Crear un widget en el sidebar para que el usuario seleccione el player_id
     selected_player_id = st.sidebar.selectbox("Choose Player", player_ids)
     
     #***********************Preparación de Consulta de Datos Personalizada y Elementos a Visualizar*************************************************************
     # Se seleccionan campos de la tabla "users" y la tabla "players".
     # Si se requieren otros campos o de otras tablas, se pueden agregar al SELECT y al JOIN.
-    sql = """
-    SELECT 
-        u.user_id,
-        u.first_name,
-        u.last_name,
-        u.birth_date,
-        u.gender,
-        u.photo_url,
-        u.phone,
-        p.player_id,
-        p.nationality,
-        p.city,
-        p.number,
-        p.dominant_foot,
-        p.primary_position,
-        p.secondary_position,
-        p.height,
-        s.player_id,            
-        s.goals,
-        s.minutes_played
-    FROM 
-        users AS u
-    JOIN 
-        players AS p ON u.user_id = p.user_id
-    JOIN
-        player_game_stats AS s ON p.player_id = s.player_id
-    WHERE
-        p.player_id = :player_id
-    """
     #****************Capturando consulta de data 360° del jugador en DataFrame********************************************************
     # --- Ejecución de la consulta ---
-    try:
-        df = dbconn.query(sql, params={"player_id": selected_player_id}, ttl=3600)
-    except Exception as e:
-        st.error(f"Error durante la consulta: {e}")
+    # try:
+    #     df = player_ids dbconn.query(sql, params={"player_id": selected_player_id}, ttl=3600)
+    # except Exception as e:
+    #     st.error(f"Error durante la consulta: {e}")
 
     #************************Preparación elementos a visualizar y otros campos calculados*************************************************************
-    if not df.empty:
+    if selected_player_id:
         # Convertir la primera fila a diccionario
-        registro = df.iloc[0].to_dict()
-        #Capturar Foto del Jugador.
-        if registro.get("photo_url"):
+        registro = session.query(Players).filter(Players.player_id == selected_player_id).first()        #Capturar Foto del Jugador.
+        if registro.user.photo_url:
             #st.image(
             #    registro["photo_url"],
             #    caption=f"Player {registro.get('first_name', 'Player')} {registro.get('last_name','')}",
@@ -151,41 +106,42 @@ def Show_Player_Info():
             st.info("No se encontró imagen para este jugador.")
         #Incluir más campos calculados como Edad + Gol Avg/90.
         # Calcular Edad
-        birth_date = registro['birth_date']
+        birth_date = registro.user.birth_date
         today = datetime.today()
         age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
         # Agregar el campo calculado al diccionario
-        registro['age'] = age
+        registro.user.age = age
+        stats = session.query(PlayerGameStats).filter_by(player_id=registro.player_id).first()
         #Calcular Gol/90
-        if registro['minutes_played'] > 0:
-            goals_90 = (registro['goals'] / registro['minutes_played']) * 90
+        if stats.minutes_played > 0:
+            goals_90 = (stats.goals / stats.minutes_played) * 90
         else:
             goals_90 = 0
         # Agregar el campo calculado al diccionario
-        registro['goals_90'] = goals_90
+        stats.goals_90 = goals_90
         #registro
 
         # Separar campos para visualizadores
         general_fields = {
-            "Last Name": registro["last_name"],
-            #"User ID": registro["user_id"],
-            "First Name": registro["first_name"],
-            "Birth Date": registro["birth_date"],
-            "Age": registro["age"],
-            "Gender": registro["gender"],
-            "Nationality": registro["nationality"],
-            "City": registro["city"],            
-            "Contact Phone": registro["phone"]
+            "Last Name": registro.user.last_name,
+            #"User ID": registro.user.user_id,
+            "First Name": registro.user.first_name,
+            "Birth Date": registro.user.birth_date,
+            "Age": registro.user.age,
+            "Gender": registro.user.gender,
+            "Nationality": registro.nationality,
+            "City": registro.city,
+            "Contact Phone": registro.user.phone
         }
         profile_fields = {
-            "Number": registro["number"],
-            "Dominant Foot": registro["dominant_foot"],
-            "Primary Position": registro["primary_position"],
-            "Secondary Position": registro["secondary_position"],
-            "Height": registro["height"],
-            "Minutes Played": registro["minutes_played"],
-            "Goals": registro["goals"],
-            "Goals/90": registro["goals_90"]
+            "Number": registro.number,
+            "Dominant Foot": registro.dominant_foot,
+            "Primary Position": registro.primary_position,
+            "Secondary Position": registro.secondary_position,
+            "Height": registro.height,
+            "Minutes Played": stats.minutes_played,
+            "Goals": stats.goals,
+            "Goals/90": stats.goals_90
         }
         
     else:
