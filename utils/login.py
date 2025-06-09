203a1853dafed845f7b6783549990b0617bd9fbc
Jose Espinoza <espinozajgx@gmail.com>
2025-06-09 16:54:31 +0200

    corregido el label del menu

--- utils/login.py
+++ utils/login.py
@@ -61,7 +61,7 @@ def generarMenu(usuario):
         st.page_link("app.py", label="Inicio", icon=":material/home:")
         st.subheader(":material/dashboard: Tableros")
         st.page_link("pages/player.py", label="360", icon=":material/contacts:")
-        st.page_link("pages/player_report_from_layout.py", label="REPORTS", icon=":material/picture_as_pdf:")
+        st.page_link("pages/player_report_from_layout.py", label="Reportes", icon=":material/picture_as_pdf:")
 
         st.subheader(":material/manage_accounts: Administrador")
 
